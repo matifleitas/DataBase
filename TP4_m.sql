1612d5c8316fc9163f2970f1b3a25e3df866d11e
Matias Fleitas <matifleitas6@gmail.com>
2023-05-16 10:20:25 -0300

    mas ejs de los tps

--- TP4_m.sql
+++ TP4_m.sql
@@ -24,15 +24,29 @@ LIMIT 30
 máximo y mínimo  no supere el 40% del sueldo máximo.
 (P) (Probar con 10% para que retorne valores) */
 
-SELECT e.id_departamento
-FROM unc_esq_peliculas.empleado e INNER JOIN
-unc_esq_peliculas.departamento d ON (d.id_departamento = e.id_departamento)
-INNER JOIN unc_esq_peliculas.tarea t ON (t.id_tarea = e.id_tarea)
-WHERE (sueldo_maximo - t.sueldo_minimo > t.sueldo_maximo * 0.4)
-GROUP BY e.id_departamento;
+SELECT e.id_departamento, d.nombre
+FROM unc_esq_peliculas.empleado e
+    JOIN unc_esq_peliculas.departamento d ON (d.id_departamento = e.id_departamento)
+        INNER JOIN unc_esq_peliculas.tarea t ON (t.id_tarea = e.id_tarea)
+        WHERE (t.sueldo_maximo - t.sueldo_minimo) < t.sueldo_maximo * 0.4
+        GROUP BY e.id_departamento, d.nombre;
 
 /*1.4. Liste las películas que nunca han sido entregadas por un distribuidor nacional.(P)*/
 
+SELECT p.codigo_pelicula, p.titulo, p.genero
+FROM unc_esq_peliculas.pelicula p
+    JOIN unc_esq_peliculas.renglon_entrega r ON (p.codigo_pelicula = r.codigo_pelicula)
+        JOIN unc_esq_peliculas.entrega e ON (r.nro_entrega = e.nro_entrega)
+        WHERE NOT EXISTS(
+            SELECT
+                1
+            FROM
+                unc_esq_peliculas.distribuidor d
+            WHERE (e.id_distribuidor=d.id_distribuidor)
+            AND (d.tipo = 'N')
+        )
+        ORDER BY p.codigo_pelicula ASC;
+---------
 SELECT codigo_pelicula, titulo
 FROM unc_esq_peliculas.pelicula p
 WHERE EXISTS (
@@ -55,30 +69,15 @@ WHERE EXISTS (
                     AND d.tipo = 'N'
                 )
         )
-    );
+    )
+    ORDER BY p.codigo_pelicula ASC;
 
 /*1.5. Determinar los jefes que poseen personal a cargo y cuyos departamentos (los del
 jefe) se encuentren en la Argentina.*/
 
 ----ARREGLAR
-SELECT e.nombre
-FROM unc_esq_peliculas.empleado e
-WHERE
-    EXISTS(
-    SELECT 1
-    FROM
-        unc_esq_peliculas.departamento d
-    WHERE (e.id_jefe=e.id_empleado)
-    AND(e.id_departamento=d.id_departamento)
-    AND
-        EXISTS(
-            SELECT
-                1
-            FROM unc_esq_peliculas.distribuidor dis
-            WHERE d.id_distribuidor=dis.id_distribuidor
-            AND dis.tipo = 'N'
-        )
---GROUP BY e.jefe_departamento;
+
+SELECT jefe_departamento
 );
 
 
@@ -91,8 +90,8 @@ del 10% de la que posee su empleado a cargo.*/
 SELECT e.nombre, e.apellido, e.id_departamento, d.jefe_departamento
 FROM unc_esq_peliculas.empleado e
     JOIN unc_esq_peliculas.departamento d
-    ON (e.id_departamento = d.id_departamento AND d.id_distribuidor = e.id_distribuidor)
-        JOIN unc_esq_peliculas.empleado e2 ON (d.jefe_departamento = e2.id_empleado)
+    ON (e.id_departamento = d.id_departamento AND d.id_distribuidor = e.id_distribuidor) --Ensamble entre departamento y empleados. Ensamble entre distribuidor y empleado
+        JOIN unc_esq_peliculas.empleado e2 ON (d.jefe_departamento = e2.id_empleado)--empleado2 es una manera de llamar al JEFE ya que es un empleado
         WHERE (d.id_ciudad) IN (SELECT c.id_ciudad
                                 FROM
                                     unc_esq_peliculas.ciudad c
@@ -111,8 +110,8 @@ SELECT p.titulo, p.genero, r.nro_entrega, e.fecha_entrega
     ON (p.codigo_pelicula = r.codigo_pelicula)
         INNER JOIN unc_esq_peliculas.entrega e
         ON (r.nro_entrega = e.nro_entrega)
-        WHERE EXTRACT(YEAR FROM e.fecha_entrega) > 2009
-        ORDER BY p.genero ASC;
+            WHERE EXTRACT(YEAR FROM e.fecha_entrega) > 2009
+            ORDER BY p.genero ASC;
 -----
 SELECT r.cantidad AS CANTIDAD_ENTREGADA, r.nro_entrega
 FROM
@@ -138,35 +137,15 @@ WHERE
 /* 1.8. Realizar un resumen de entregas por día, indicando el video club al cual se le
 realizó la entrega y la cantidad entregada. Ordenar el resultado por fecha.*/
 
-SELECT e.fecha_entrega, v.propietario AS video_club, COUNT(*) AS cantidad_entregada
-FROM unc_esq_peliculas.entrega e
-    INNER JOIN unc_esq_peliculas.video v ON (e.id_video = v.id_video)
-    GROUP BY e.fecha_entrega, v.propietario
-    ORDER BY e.fecha_entrega;
+SELECT e.fecha_entrega, e.id_video, SUM(r.cantidad) AS cantidad_de_peliculas
+    FROM unc_esq_peliculas.entrega e
+        JOIN unc_esq_peliculas.renglon_entrega r ON (e.nro_entrega=r.nro_entrega)
+        GROUP BY  e.fecha_entrega, e.id_video
+        ORDER BY e.fecha_entrega, e.id_video;
 
 /* 1.9 - Listar, para cada ciudad, el nombre de la ciudad y la cantidad de empleados
 mayores de edad que desempeñan tareas en departamentos de la misma y que posean al
 menos 30 empleados.*/
-
-SELECT c.nombre_ciudad, c.id_ciudad, count(*) AS CANTIDAD_EMPLEADOS
-FROM unc_esq_peliculas.ciudad c
-    JOIN unc_esq_peliculas.departamento d ON (c.id_ciudad = d.id_ciudad)
-        JOIN unc_esq_peliculas.empleado e ON d.id_distribuidor = e.id_distribuidor AND d.id_departamento = e.id_departamento
-
-WHERE EXTRACT(year FROM AGE(e.fecha_nacimiento))>18
-GROUP BY c.nombre_ciudad, c.id_ciudad
-HAVING count(*)>30; --HAVING, ES EL WHERE DEL GROUP BY
-LIMIT 20;
-
------PREGUNTAR
-
-SELECT c.nombre_ciudad,  COUNT(*) AS cantidad_empleados
-FROM unc_esq_peliculas.ciudad c
-    INNER JOIN unc_esq_peliculas.departamento d ON (c.id_ciudad = d.id_ciudad)
-        INNER JOIN unc_esq_peliculas.empleado e ON (d.id_departamento = e.id_departamento)
-        WHERE EXTRACT(YEAR FROM e.fecha_nacimiento)<'1988' AND e.id_tarea<'31'
-        GROUP BY c.nombre_ciudad
-        ORDER BY c.nombre_ciudad ASC;
 
 /*Manera correcta*/
 
@@ -180,7 +159,7 @@ SELECT c.nombre_ciudad, count(*)
             IN
               (SELECT dep.id_distribuidor, dep.id_departamento
                 FROM unc_esq_peliculas.departamento as dep
-                INNER JOIN unc_esq_peliculas.empleado as emp ON (dep.id_distribuidor = emp.id_distribuidor) and (dep.id_departamento = emp.id_departamento)
+                INNER JOIN unc_esq_peliculas.empleado as emp ON (dep.id_distribuidor = emp.id_distribuidor)and(dep.id_departamento = emp.id_departamento)
                 WHERE EXTRACT(year FROM AGE(emp.fecha_nacimiento)) > 18
                 GROUP BY (dep.id_distribuidor, dep.id_departamento)
                 HAVING count(*) >= 30)
@@ -191,28 +170,23 @@ SELECT c.nombre_ciudad, count(*)
 /*2.1 Muestre, para cada institución, su nombre y la cantidad de voluntarios que realizan
 aportes. Ordene el resultado por nombre de institución.*/
 
-SELECT i.id_institucion, nombre_institucion, v.nro_voluntario, v.apellido, t.nombre_tarea, t.id_tarea
+SELECT i.id_institucion, i.nombre_institucion, COUNT(v.nro_voluntario) AS cantidad_de_voluntarios
 FROM unc_esq_voluntario.institucion i
-    INNER JOIN  unc_esq_voluntario.voluntario v
-    ON(i.id_institucion = v.id_institucion)
-        INNER JOIN unc_esq_voluntario.tarea t
-        ON(t.id_tarea = v.id_tarea)
-            ORDER BY nombre_institucion ASC
+    JOIN unc_esq_voluntario.voluntario v ON (i.id_institucion = v.id_institucion)
+        GROUP BY i.id_institucion, i.nombre_institucion
+        ORDER BY i.nombre_institucion;
 
 /*2.2. Determine la cantidad de coordinadores en cada país, agrupados por nombre de
 país y nombre de continente. Etiquete la primer columna como 'Número de coordinadores' */
 
-SELECT COUNT(*) AS Numero_de_coordinadores, p.nombre_pais, c.nombre_continente
+SELECT COUNT(v.id_coordinador) AS Número_de_coordinadores, p.nombre_pais, c.nombre_continente
 FROM unc_esq_voluntario.continente c
-    INNER JOIN unc_esq_voluntario.pais p
-    ON (c.id_continente = p.id_continente)
-        INNER JOIN unc_esq_voluntario.direccion d
-        ON(p.id_pais = d.id_pais)
-            INNER JOIN unc_esq_voluntario.institucion i
-            ON(d.id_direccion = i.id_direccion)
-                INNER JOIN unc_esq_voluntario.voluntario v
-                ON (i.id_institucion = v.id_institucion)
+    JOIN unc_esq_voluntario.pais p ON (c.id_continente = p.id_continente)
+        JOIN unc_esq_voluntario.direccion d ON (p.id_pais = d.id_pais)
+            JOIN unc_esq_voluntario.institucion i ON (d.id_direccion = i.id_direccion)
+                JOIN unc_esq_voluntario.voluntario v ON i.id_institucion = v.id_institucion
                 GROUP BY p.nombre_pais, c.nombre_continente
+                ORDER BY p.nombre_pais;
 
 /*2.3. Escriba una consulta para mostrar el apellido, nombre y fecha de nacimiento de
 cualquier voluntario que trabaje en la misma institución que el Sr. de apellido Zlotkey.
@@ -220,8 +194,19 @@ Excluya del resultado a Zlotkey.*/
 
 SELECT v.apellido, v.nombre, v.fecha_nacimiento, i.nombre_institucion
 FROM unc_esq_voluntario.voluntario v
+    JOIN unc_esq_voluntario.institucion i ON (i.id_institucion=v.id_institucion)
+    WHERE (i.nombre_institucion = (SELECT
+               *
+           FROM unc_esq_voluntario.voluntario v
+           WHERE (v.apellido = 'Zlotkey')
+          ));
+
+--NOC COMO HACERLO PARA QUE DEVUELVA EL NAME DE LA INSTITUCION
+
+SELECT v.apellido, v.nombre, v.fecha_nacimiento, i.nombre_institucion
+FROM unc_esq_voluntario.voluntario v
     INNER JOIN unc_esq_voluntario.institucion i ON (v.id_institucion = i.id_institucion)
-    WHERE i.nombre_institucion = 'FUNDACION REGAZO' AND v.apellido NOT IN ('Zlotkey')
+    WHERE i.nombre_institucion = 'FUNDACION REGAZO' AND v.apellido NOT IN ('Zlotkey');
 
 /*2.4. Cree una consulta para mostrar los números de voluntarios y los apellidos de todos
 los voluntarios cuya cantidad de horas aportadas sea mayor que la media de las horas
---
+++ Tp5.sql
@@ -0,0 +1,254 @@
+CREATE TABLE matute_prueba (
+    nombre VARCHAR(100) NOT NULL,
+    id int NOT NULL,
+    descripcion VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_ID PRIMARY KEY (id));
+
+/* 1)a)Cómo debería implementar las Restricciones de Integridad Referencial (RIR) si se desea que cada vez que se elimine un
+   registro de la tabla PALABRA , también se eliminen los artículos que la referencian en la tabla CONTIENE.*/
+
+ALTER TABLE unc_251672.p5p1e1_palabra  ADD CONSTRAINT FK_CONTIENE
+    FOREIGN KEY (idioma, cod_palabra)
+    REFERENCES unc_251672.p5p1e1_contiene (idioma, cod_palabra)
+    ON DELETE CASCADE;
+
+/*Verifique qué sucede con las palabras contenidas en cada artículo, al eliminar una palabra,
+si definen la Acción Referencial para las bajas (ON DELETE) de la RIR correspondiente
+como:
+    ii) Restrict
+    iii) Es posible para éste ejemplo colocar SET NULL o SET DEFAULT para ON
+    DELETE y ON UPDATE?*/
+
+INSERT INTO unc_251672.p5p1e1_palabra (idioma, cod_palabra, descripcion) VALUES ('2', '445667', 'vb dsffsdffsdasdf jskodjk');
+
+/* 2)
+a) Indique el resultado de las siguientes operaciones, teniendo en cuenta las acciones
+referenciales e instancias dadas. En caso de que la operación no se pueda realizar, indicar qué
+regla/s entra/n en conflicto y cuál es la causa. En caso de que sea aceptada, comente el
+resultado que produciría (NOTA: en cada caso considere el efecto sobre la instancia original de
+la BD, los resultados no son acumulativos).*/
+
+--b.1)
+delete from tp5_p1_ej2_proyecto where id_proyecto = 3;
+--INSERT INTO unc_251672.tp5_p1_ej2_proyecto (id_proyecto, nombre_proyecto, anio_inicio, anio_fin) VALUES ('3', 'Proy 3', '2020', NULL);
+
+--b.2)
+update tp5_p1_ej2_proyecto set id_proyecto = 3 where id_proyecto = 7;
+
+--b.3)
+delete from tp5_p1_ej2_proyecto where id_proyecto = 1;
+--No se puede borrar porque en la tabla "trbaja_en" ya esta seteado el id_proyecto = 1, no deberia haber registros
+--referenciados a id_proyecto = 1 para que se pueda realizar;
+
+--b.4)
+delete from tp5_p1_ej2_empleado where tipo_empleado = 'A' and nro_empleado = 2;
+--INSERT INTO tp5_p1_ej2_empleado VALUES ('A ', 2, 'María', 'Casio', 'CIO');
+
+--b.5)
+update tp5_p1_ej2_trabaja_en set id_proyecto = 1 where id_proyecto =3;
+
+--b.6)
+update tp5_p1_ej2_proyecto set id_proyecto = 5 where id_proyecto = 2;
+--No se puede hacer el update ya que id_proyecto esta asociado a la tabla TRABAJA_EN al empleado 2 y a la tabla
+--AUSPICIO a McDonald;
+
+--b)
+
+
+--d)
+--.a)
+    insert into unc_251672.tp5_p1_ej2_auspicio values (1, 'Dell' , 'B', null); --es rechazada, B (tipo_empleado) existe pero
+    --viola la fk que en la tabla empleado debe existir un nro de empleado para que se pueda insertar en auspicio
+
+--.b)
+    insert into unc_251672.tp5_p1_ej2_auspicio values (2, 'Oracle', null, null); --matching full
+
+--.c)
+    insert into unc_251672.tp5_p1_ej2_auspicio values (3, 'Google', 'A', 3); --no existe el empleado = 3
+
+--.d)
+    insert into unc_251672.tp5_p1_ej2_auspicio values (1, 'HP', null, 3); --no existe el empleado = 3
+
+--EJERCICIO 3
+
+
+
+
+
+
+--EJERCICIO 4
+--1)a) No se puede. Ya esta registrada como foreign key en la tabla
+--b) F el alter table, tendria que ser nombrado en auto y en vez de conductor, ser contacto ya que en la tabla equipo
+-- no existe el atributo conductor
+--c) F ya que etapa no existe como atributo en la tabla etapa.
+--d) F debido a que nro_auto, es primary key de auto y no de la tabla etapa.
+--e) V id_equipo es la primary key de la tabla equipo correctamente.
+--f) V id_equipo y nro_auto son claves correctas para aplicarse como fk en dempEtapa
+
+
+
+
+--TP5-PARTE_2
+
+--3)A)
+-- A. Controlar que las nacionalidades sean 'Argentina' 'Español' 'Inglés' 'Alemán' o 'Chilena'.
+-- TIPO - check atributo = dominio
+      ALTER TABLE unc_251672.p5p1e1_articulo
+      ADD CONSTRAINT ck_articulo_nacionalidad
+      CHECK (nacionalidad IN ( --NO ESTA CREADO EL ATRIBUTO NACIONALIDAD EN LA TABLA ARTICULO
+          'Argentina', 'Español', 'Inglés', 'Alemán', 'Chilena'
+          ));
+
+--3)B)
+-- B. Para las fechas de publicaciones se debe considerar que sean fechas posteriores o iguales al 2010.
+-- TIPO TIPO - check atributo = dominio
+    ALTER TABLE unc_251672.p5p1e1_articulo
+    ADD CONSTRAINT ck_fecha_publicacion_mayores_a2010
+    CHECK (
+        EXTRACT(YEAR FROM fecha_publicacion) >= 2010 --NO ESTA CREADO EL ATRIBUTO FECHA_PUBLICACION EN LA TABLA ARTICULO
+        );
+
+--3)C)
+--C. Cada palabra puede aparecer como máximo en 5 artículos.
+-- buscar las palabras que aparecen en más de 5 articulos
+-- TIPO - TABLA
+
+    ALTER TABLE unc_251672.p5p1e1_contiene
+    ADD CONSTRAINT ck_cant_max_palabra
+    CHECK ( NOT EXISTS (
+                SELECT 1
+                FROM Contiene c
+                GROUP BY c.idioma, c.cod_palabra
+                HAVING COUNT(*) > 5
+            ));
+
+--3)D)
+--D. Sólo los autores argentinos pueden publicar artículos que contengan más de
+-- 10 palabras claves, pero con un tope de 15 palabras, el resto de los autores
+-- sólo pueden publicar artículos que contengan hasta 10 palabras claves.
+-- TIPO - general
+
+CREATE ASSERTION autores_argentinos
+CHECK (NOT EXISTS(
+        SELECT 1
+        FROM Articulo a
+        WHERE (nacionalidad LIKE 'Argentina' AND
+                id_articulo IN ( SELECT 1
+                                 FROM Contiene
+                                 GROUP BY id_articulo
+                                 HAVING COUNT(*) > 15) ) OR
+                                (nacionalidad NOT LIKE 'Argentina' AND
+                                 id_articulo IN(SELECT 1
+                                                FROM contiene
+                                                GROUP BY id_articulo
+                                                HAVING COUNT(*) > 10) ))
+    );
+
+--4)A)
+    --TIPO - ATRIBUTO
+    ALTER TABLE unc_251672.p5p2e4_imagen_medica im
+    ADD CONSTRAINT ck_valores_imagen_medica
+    CHECK ( im.modalidad IN (
+            'RADIOLOGIA', 'CONVENCIONAL', 'FLUOROSCOPIA', 'ESTUDIOS RADIOGRAFICOS CON
+            FLUOROSCOPIA', 'MAMOGRAFIA', 'SONOGRAFIA',
+        ) );
+
+--B)
+    -- TIPO TABLA
+    ALTER TABLE unc_251672.p5p2e4_procesamiento
+    ADD CONSTRAINT ck_limite_imagenes
+    CHECK ( NOT EXISTS(
+            SELECT 1
+            FROM procesamiento p
+            GROUP BY id_imagen, id_paciente
+            HAVING COUNT(*) > 5
+    ) );
+
+--C)
+    --TIPO GENERAL
+    ALTER TABLE unc_251672.p5p2e4_imagen_medica --Esta es la sintaxsis de agregacion de columnas
+    ADD COLUMN fecha_imagen date;
+
+    ALTER TABLE unc_251672.p5p2e4_procesamiento --Esta es la sintaxsis de agregacion de columnas
+    ADD COLUMN fecha_procesamiento date;
+-----
+    CREATE ASSERTION
+    CHECK (
+        NOT EXISTS( SELECT 1
+                    FROM imagen_medica i JOIN procesamiento p
+                    ON ((i.id_paciente = p.id_paciente) AND (i.id_imagen = p.id_imagen))
+                    WHERE i.fecha_imagen > p.fecha_procesamiento
+    );
+
+--D)
+    ALTER TABLE unc_251672.p5p2e4_imagen_medica
+    ADD CONSTRAINT ck_limite_procesamientos
+    CHECK (NOT EXISTS(
+            SELECT 1
+            FROM imagen_medica
+            WHERE (modalidad LIKE 'FLUOROSCOPIA')
+            GROUP BY id_paciente, EXTRACT(YEAR FROM fecha_img)
+            HAVING (*) > 2
+        ));
+
+   ALTER TABLE p5p2e4_imagen_medica
+   ADD CONSTRAINT CK_CANTIDAD_PROCESAMIENTOS
+   CHECK ( NOT EXISTS (
+                SELECT 1
+                FROM p5p2e4_imagen_medica
+                WHERE modalidad = 'FLUOROSCOPIA'
+                GROUP BY id_paciente, extract(year from fecha_img)
+                HAVING COUNT(*) > 2 ))
+;
+
+--E)
+    CREATE ASSERTION ASDAS
+    CHECK (NOT EXISTS (
+                SELECT 1
+                FROM algoritmo a
+                WHERE costo_computacional = 'O(n)'
+                AND id_algoritmo IN (SELECT 1
+                                     FROM procesamiento p JOIN imagen_medica i
+                                     ON ((p.id_paciente = i.id_paciente) AND (i.id_imagen = p.id_imagen))
+                                     WHERE i.modalidad = 'FLUOROSCOPIA'
+                                    )
+    ));
+    ---
+    CREATE ASSERTION
+       CHECK ( NOT EXISTS (
+                    SELECT 1
+                    FROM p5p2e4_imagen_medica i JOIN p5p2e4_procesamiento p ON
+                    (i.id_paciente = p.id_paciente AND i.id_imagen = p.id_imagen)
+                    JOIN p5p2e4_algoritmo a ON ( p.id_algoritmo = a.id_algoritmo )
+                    WHERE modalidad = 'FLUOROSCOPIA' AND
+                    costo_computacional = 'O(n)'
+));
+
+
+--5)-A)
+        ALTER TABLE venta
+        ADD CONSTRAINT CK_DECUENTO
+        CHECK( descuento IN
+            (BETWEEN 0 AND 100)));
+
+--B)
+--TABLA
+    ALTER TABLE VENTA
+    ADD CONSTRAINT  CK_DESCUENTO_FECHA
+    CHECK ( NOT EXISTS(
+                SELECT 1
+                FROM p5p2e5_venta
+                GROUP BY descuento, EXTRACT(YEAR FROM fecha)
+                HAVING COUNT(*) < 30
+    ));
+
+--C)
+    ALTER TABLE
+
+
+--D)
+    CREATE ASSERTION OFERTA_SIN_DESCUENTO
+    CHECK (NOT EXISTS (SELECT 1
+                        FROM prenda p JOIN venta v ON (p.id_prenda = v.id_prenda)
+                        WHERE p.categoria = 'oferta' AND v.descuento IS NOT NULL));
+
